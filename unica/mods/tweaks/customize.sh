d82148523ef28e132fc820167f5d2f8792199c2c
Mehedi H Joy <mh506370@gmail.com>
2026-09-06 05:47:38 +0000

    unica/mods/tweaks: allow doze for all apps
    Signed-off-by: Mehedi H Joy
    <mh506370@gmail.com>

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -8,3 +8,12 @@ APPLY_PATCH "system" "system/framework/services.jar" \
 if [ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION")" ]; then
     SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
 fi
+
+LOG_STEP_IN "- Applying extreme doze for all apps"
+EVAL "find \"\$WORK_DIR\" -type f \( -path '*/etc/permissions/*.xml' -o -path '*/etc/sysconfig/*.xml' \) -exec \
+    sed -i -E '/<(allow-(in-(power|data-usage)-save(-except-idle)?|(unthrottled|ignore)-(location|alarms?)(-settings)?|implicit-broadcast|background-activity-starts|auto-restarter|in-app-standby)|(bg-restriction|app-standby|system|cached-app-freezer)-exemption|system-component|auto-start-whitelist)[[:space:]]/d' {} +"
+
+EVAL "sed -i '/<reviewed-in-power-save/d' \"\$WORK_DIR\"/system/system/etc/deviceidle/reviewed_allowlist.xml"
+LOG_STEP_OUT
+
+
